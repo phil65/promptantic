cab79286b6b16337e594f39eda3717f1d44a3493
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-11 01:32:36 +0100

    Bump version 0.3.1 → 0.3.2

--- src/promptantic/__init__.py
+++ src/promptantic/__init__.py
@@ -5,6 +5,6 @@ from __future__ import annotations
 from promptantic.generator import ModelGenerator
 from promptantic.exceptions import PromptanticError
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 __all__ = ["ModelGenerator", "PromptanticError"]
